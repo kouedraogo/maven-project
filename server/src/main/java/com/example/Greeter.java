0975158186a05de3dfe7d5406916a7dc263a75e0
kouedraogo <80824274+kouedraogo@users.noreply.github.com>
2021-07-02 15:09:00 +0200

    Update Greeter.java
    add javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,9 +3,14 @@ package com.example;
 
 public class Greeter {
 
-  
+  /**
+         * Ceci est un commentaire Javadoc. 
+         * Il commence par un slash suivis de deux étoiles. 
+         * Chaque ligne doit ensuite commencer par une étoile. 
+         * Enfin, il fini par une étoile suivie d'un slash.
+   */
   public Greeter() {
-  //Methode greeter
+ 
   }
 
   public final String greet(final String someone) {
