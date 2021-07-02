608396c5a5d26c891efc3c7d8e0b7b5f4f8103e2
kouedraogo <80824274+kouedraogo@users.noreply.github.com>
2021-07-02 15:18:24 +0200

    Update Greeter.java
    add javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
  
   }
   
-  //TODO: Add javadoc comment
+  /**
+  * @param someone name of a person
+  * @return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
